e41aa3ffc2409c615c2f68f31236ceb9f33e68fa
Joe Hawk <azharrizkizulma@gmail.com>
2022-06-22 17:56:29 +0700

    test c & c++

---
+++ test.cpp
@@ -0,0 +1,8 @@
+// Create bubblesort function
+void main () {
+    int a[10] = {1,2,3,4,5,6,7,8,9,10};
+    bubblesort(a, 10);
+    for (int i = 0; i < 10; i++) {
+        cout << a[i] << " ";
+    }
+}
