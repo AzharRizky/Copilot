814cd9230e07e685bdba9f6e150c9fd7b8c34c94
Joe Hawk <azharrizkizulma@gmail.com>
2022-06-26 14:23:07 +0700

    Update test.php

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+
+// Create some class to write message & calculate number
+class MyClass {
+    public function write_message($message) {
+        echo $message;
+    }
+
+    public function calculate_number($number) {
+        return $number * 2;
+    }
+}
